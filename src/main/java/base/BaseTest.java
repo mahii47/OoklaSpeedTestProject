ae37e055d41f77ecf49c69e2ba07e7966c9a9784
mahii47 <mahmankar333@gmail.com>
2025-10-25 16:49:35 +0530

    Update Code

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,24 +1,26 @@
 package base;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
 import pages.SpeedTestPage;
 
 public class BaseTest {
 	
 	public static WebDriver driver;
 	protected SpeedTestPage speedpage;
-	
+	public static final Logger logger = LogManager.getLogger(BaseTest.class);
 	@BeforeTest
 	public void setup()
 	{
+		logger.info("Starting Speed Test Automation");
 		driver = new ChromeDriver();
 		speedpage = new SpeedTestPage(driver);
 		driver.manage().window().maximize();
 		driver.get("https://www.speedtest.net/");
+		logger.info("Opening Speedtest website");
 	}
 	@AfterTest
 	public void teardown()
